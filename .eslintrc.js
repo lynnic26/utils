84f3762f139e20ab7db1bb236c32c548323d9578
lynnic26 <kwanic@qq.com>
2017-09-15 23:42:39 +0800

    chore(eslintrc): allow debugger during development

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
         // https://github.com/standard/standard/blob/master/docs/RULES-zhcn.md
         'semi': ['error', 'always'],
         'indent': ['error', 2],
-        'quotes': ['error', 'single']
+        'quotes': ['error', 'single'],
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
     }
 };
